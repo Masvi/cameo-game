5f6556575f35df60e5241e8033e0d626c9ce03ae
masvi <jeanmasvi@gmail.com>
2022-10-27 07:06:12 -0300

    feat: added views

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,26 @@
 <script>
-  
+  import Welcome from "./views/Welcome.svelte";
+
+  let state = "welcome";
 </script>
 
-
 <main>
-  <div class="home">
-  </div>
+  {#if state === "welcome"}
+    <Welcome />
+  {:else if state === "playing"}
+    <p>playing</p>
+  {/if}
 </main>
 
 <style lang="scss">
-$black: #000;
-
-.home {
-  width: 1000px;
-  height: 800px;
-  background-color: $black;
-}
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    height: 100%;
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 1rem;
+  }
 </style>
---
+++ src/views/Welcome.svelte
@@ -0,0 +1,23 @@
+<script>
+</script>
+
+<div class="welcome">
+  <h1>CAMEO GAME</h1>
+  <div class="welcome__subtitle">
+    <p>
+      On <a target="_blank" href="https://www.cameo.com/" rel="noreferrer"
+        >cameo.com</a
+      >
+      you can buy personalised video clips from everyone from Lindsay Lohan to Ice
+      T. But who commands the highest price? Pick a category to play a game:
+    </p>
+  </div>
+</div>
+
+<style lang="scss">
+  .welcome {
+    &__subtitle {
+      padding: 1rem;
+    }
+  }
+</style>
