9ff09314f0076fcae15961b003a367be88cadf2b
masvi <jeanmasvi@gmail.com>
2022-10-28 06:57:52 -0300

    feat: added button component

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,5 +1,30 @@
-<button><slot></slot></button>
+<script>
+  export let onClick;
+</script>
+
+<button class="base-button" on:click={onClick}>
+  <slot />
+</button>
 
 <style lang="scss">
-    
+  .base-button {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    margin: 2px;
+    cursor: pointer;
+    transition: border-color 0.25s;
+  }
+
+  button:hover {
+    border-color: #646cff;
+  }
+
+  button:focus,
+  button:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+  }
 </style>
--- src/views/Welcome.svelte
+++ src/views/Welcome.svelte
@@ -1,15 +1,23 @@
 <script>
+  import { createEventDispatcher } from "svelte";
   import Button from "../components/Button.svelte";
 
   const categories = [
-    { id: 1, name: "Actors" },
-    { id: 2, name: "Athletes" },
-    { id: 3, name: "Comedians" },
-    { id: 4, name: "Creators" },
-    { id: 5, name: "Models" },
-    { id: 6, name: "Musicians" },
-    { id: 7, name: "Reality TV" },
+    { slug: "actors", label: "Actors" },
+    { slug: "athletes", label: "Athletes" },
+    { slug: "comedians", label: "Comedians" },
+    { slug: "creators", label: "Creators" },
+    { slug: "models", label: "Models" },
+    { slug: "musicians", label: "Musicians" },
+    { slug: "reality_tv", label: "Reality TV" },
   ];
+
+  const dispatch = createEventDispatcher();
+
+  const handleSelectCategory = (category) => {
+    console.log('here....')
+    console.log(category);
+  }
 </script>
 
 <div class="welcome">
@@ -19,21 +27,29 @@
       On <a target="_blank" href="https://www.cameo.com/" rel="noreferrer"
         >cameo.com</a
       >
-      you can buy personalised video clips from everyone from Lindsay Lohan to Ice
+      you can buy personalised vslugeo""clips from everyone from Lindsay Lohan to Ice
       T. But who commands the highest price? Pick a category to play a game:
     </p>
   </div>
-  <div>
+  <div class="welcome__categories">
     {#each categories as category}
-      <Button>{category.name}</Button>
+      <Button onClick={handleSelectCategory}>{category.label}</Button>
     {/each}
   </div>
 </div>
 
 <style lang="scss">
   .welcome {
+    display: flex;
+    flex-flow: column wrap;
+    align-items: center;
+
     &__subtitle {
       padding: 1rem;
+    }
+
+    &__categories {
+      max-width: 450px;
     }
   }
 </style>
